6f8cd90d8a8fdb639efb79043d4ed0316327a2e2
AndroPlus <mail@andro.plus>
2022-09-20 10:06:56 +0900

    Force remove CN keyboard

--- customize.sh
+++ customize.sh
@@ -1,2 +1,5 @@
 REPLACE="
+/system/product/data-app/BaiduIME
+/system/product/data-app/com.iflytek.inputmethod.miui
+/system/app/SogouInput
 "
---
+++ post-fs-data.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+
+
+# ime enable --user 0 com.google.android.inputmethod.latin/com.android.inputmethod.latin.LatinIME
+# ime set --user 0 com.google.android.inputmethod.latin/com.android.inputmethod.latin.LatinIME
